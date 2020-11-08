2ade67f1f5e61589dc37124b8bbcc128a610b75a
Sashko <cawacell@mail.ru>
2020-11-08 22:15:10 +0300

    сделал что надо

--- AccountingSystemUI/AccountingSystemUI/ESMInDepotPage.xaml.cs
+++ AccountingSystemUI/AccountingSystemUI/ESMInDepotPage.xaml.cs
@@ -22,7 +22,43 @@ namespace AccountingSystemUI
     {
         public ESMInDepotPage()
         {
-            InitializeComponent();
+            InitializeComponent();           
+        }
+
+        static DockPanel DockPanelForESMList(string ESMNumber)
+        {
+            DockPanel dp = new DockPanel();
+            Label label = new Label();
+            label.FontSize = 20;
+            label.Content = "ЭНИ №" + ESMNumber + "   ";
+            dp.Children.Add(label);
+            Button btn = new Button();
+            btn.FontSize = 20;
+            btn.Content = "Подробнее";
+            dp.Children.Add(btn);
+
+            return dp;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ESMList.Items.Clear();// при нажатии на кнопку ListBox отчищается от предыдущих заполнений
+            var button = sender as Button;
+            button.IsEnabled = false;
+            switch (button.Tag)
+            {
+                
+                case "0":  break;
+                case "1":  break;
+                case "2":  break;
+            }
+
+            // можно добавть цикл или изменить метод
+            String ESMNumber = "123456";
+            ESMList.Items.Add(DockPanelForESMList(ESMNumber));
+            ESMNumber = "654321";
+            ESMList.Items.Add(DockPanelForESMList(ESMNumber));
+
+            button.IsEnabled = true;
         }
     }
 }
